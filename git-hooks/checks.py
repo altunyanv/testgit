31083ded4ba76315d99461779001c4d79636258d
altunnyanv <altunnyanv@gmail.com>
2020-03-02 23:19:24 +0400

    Ok

---
+++ git-hooks/checks.py
@@ -0,0 +1,7 @@
+from git import Repo
+
+def check_for_file_size(repo : Repo):
+    return 1
+
+def check_for_duplicate_task(repo : Repo):
+    return 0
---
+++ git-hooks/pre-commit.py
@@ -0,0 +1,18 @@
+import checks
+from git import Repo
+
+custom_checks = ['check_for_file_size', 'check_for_duplicate_task']
+
+if __name__ == "__main__":
+    repo = Repo('.')
+    ok = True
+    for check in custom_checks:
+        response = 0
+        response = eval(f'checks.{check}(repo)')
+        if response == 1:
+            print(f"Check of {check} failed!")
+            ok = False
+    
+    if not ok:
+        print("Commit has been failed!")
+    exit(0 if ok else 1)
